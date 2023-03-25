79458f0bc7f7e5fc81d92b86db5deb45d1b65bde
fabricio22br <fabriciocostaodonto@gmail.com>
2023-03-25 12:19:43 -0300

    feat: add interactive from add favorite and remove favorite

--- src/components/atoms/Button.js
+++ src/components/atoms/Button.js
@@ -1,12 +1,16 @@
 import { Button as ChakraButton } from '@chakra-ui/react'
 
-export const Button = ({ children, ...props }) => (
+export const Button = ({ children, secondary, ...props }) => (
   <ChakraButton
     fontWeight="bold"
     fontSize="16"
     borderRadius="16px"
     h="56px"
-    bg="brand.primary"
+    bg={secondary ? 'brand.greyDark' : 'brand.primary'}
+    _hover={{
+      bg: secondary ? 'brand.greyDark' : 'brand.primary'
+    }}
+    textColor={secondary ? 'brand.white' : 'brand.black'}
     {...props}
   >
     {children}
