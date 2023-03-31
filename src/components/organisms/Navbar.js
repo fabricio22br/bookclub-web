908e25b3156f14a6ee2fd03eab2ea1f7d95e6ab2
fabricio22br <fabriciocostaodonto@gmail.com>
2023-03-31 14:44:52 -0300

    feat: add information author and bar search

--- src/components/organisms/Navbar.js
+++ src/components/organisms/Navbar.js
@@ -2,7 +2,7 @@ import { Flex, Image } from '@chakra-ui/react'
 import { SearchBar, UserMenu } from 'components/molecules'
 import { useNavigate } from 'react-router-dom'
 
-export const Navbar = () => {
+export const Navbar = ({ query, setQuery }) => {
   const navigate = useNavigate()
   return (
     <Flex
@@ -22,7 +22,7 @@ export const Navbar = () => {
         onClick={() => navigate('/home')}
       />
       <Flex display={['none', 'flex']}>
-        <SearchBar />
+        <SearchBar query={query} setQuery={setQuery} />
       </Flex>
       <UserMenu />
     </Flex>
